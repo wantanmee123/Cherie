98d750470afa565e98ea4a20c54106e2b90ef85f
wantanmee123 <mengkeonglee55@gmail.com>
2020-08-24 22:50:23 +0800

    initial commit for home page

--- app/src/main/java/com/example/cherie/Home.kt
+++ app/src/main/java/com/example/cherie/Home.kt
@@ -2,10 +2,13 @@ package com.example.cherie
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.cherie.databinding.ActivityHomeBinding
 
 class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        @Suppress("UNUSED_VARIABLE")
+        val binding = DataBindingUtil.setContentView<ActivityHomeBinding>(this, R.layout.activity_home)
     }
 }
