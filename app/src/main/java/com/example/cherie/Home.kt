876bbde3d05791cc33d83838f0965b59a76ebd7e
wantanmee123 <mengkeonglee55@gmail.com>
2020-09-03 02:21:02 +0800

    third commit for update home page(drawable menu and site menu)

--- app/src/main/java/com/example/cherie/Home.kt
+++ app/src/main/java/com/example/cherie/Home.kt
@@ -3,12 +3,29 @@ package com.example.cherie
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.cherie.databinding.ActivityHomeBinding
+
+private lateinit var drawerLayout: DrawerLayout
 
 class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityHomeBinding>(this, R.layout.activity_home)
+
+        drawerLayout = binding.drawerLayout
+
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this,navController, drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
